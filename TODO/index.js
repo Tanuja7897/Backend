66eae3255d00d5a3e92db9cda3c8137573b14009
Tanuja <tanuja7897@gmail.com>
2025-05-04 15:43:52 +0530

    add some verification logic

--- TODO/index.js
+++ TODO/index.js
@@ -2,96 +2,102 @@ const { isUtf8 } = require('buffer');
 const express = require('express');
 const app = express();
 app.use(express.json());
-const fs  = require("fs");
+const fs = require("fs");
 const { dirname } = require('path');
-let path = __dirname+"/todo.json"
+let path = __dirname + "/todo.json"
 //temp db
 let todo = []
 
-app.get('/' , (req , res) =>{
-    return res.status(200).json({"message" : 'kya hal hai'})
+app.get('/', (req, res) => {
+    return res.status(200).json({ "message": 'kya hal hai' })
 })
 
-app.get('/todo' , (req , res)=>{
+app.get('/todo', (req, res) => {
     //we can use map function also but this also works kyoki hmne pura array hi print krva liya h 
     //when we use map funtion then output indivisual {} obj ayega
-    try{
-        fs.readFile(path, {encoding : "utf-8"},(err , todo)=>{
+    try {
+        const userid = req.headers.userid ;
+        fs.readFile(path, { encoding: "utf-8" }, (err, todo) => {
             todo = todo ? JSON.parse(todo) : []
-            return res.status(200).json({todo : todo}); //json.parse because we are sending data in form of JSON, not string
+            todo = todo.filter(todo => todo.userid == userid)
+            return res.status(200).json({ todo: todo }); //json.parse because we are sending data in form of JSON, not string
         })
-    }catch(err){
-        return res.status(500).json({"message" : "Please try again"});
+    } catch (err) {
+        return res.status(500).json({ "message": "Please try again" });
     }
-    
+
 })
 
-app.post('/todo' , (req, res) =>{
-    try{
-        fs.readFile(path , {encoding : "utf-8"} , (err , todo)=>{
+app.post('/todo', (req, res) => {
+    //for different users cards wiill be different .. so when user hit addtodo button i will send userd id also with the header
+    try {
+        const userid = req.headers.userid ;
+        fs.readFile(path, { encoding: "utf-8" }, (err, todo) => {
             todo = todo ? JSON.parse(todo) : []
-            todo.push({...req.body , ischecked : false , id : todo.length + 1})
-            fs.writeFile(path , JSON.stringify(todo) , {encoding : "utf-8"} , (err)=>{
-                if(err)
-                {
-                    return res.status(500).json({"message" : "Please try again"});   
+            todo.push({ ...req.body, ischecked: false, id: todo.length + 1 ,userid})
+            fs.writeFile(path, JSON.stringify(todo), { encoding: "utf-8" }, (err) => {
+                if (err) {
+                    return res.status(500).json({ "message": "Please try again" });
                 }
-                else{
-                    return res.status(200).json({"status" : "done"})
+                else {
+                    return res.status(200).json({ "status": "done" })
                 }
             })
-    })
-    }catch(err){
-        return res.status(500).json({"message" : "Please try again"});     
+        })
+    } catch (err) {
+        return res.status(500).json({ "message": "Please try again" });
     }
-    
+
 })
 
-app.delete('/todo/:id' , (req,res)=>{
-    try{
-            fs.readFile(path , {encoding : "utf-8"} , (err , todo)=>{
+app.delete('/todo/:id', (req, res) => {
+    //will do indivisua user card delete in mongodb
+    const userid = req.headers.userid ;
+    if(!userid){
+        return res.json({"message":"please login!"})
+    }
+    try {
+        fs.readFile(path, { encoding: "utf-8" }, (err, todo) => {
             //prefer
             todo = todo ? JSON.parse(todo) : []
-            filtertodo = todo.filter(todo => todo.id != req.params.id);
+            filtertodo = todo.filter(todo => todo.id != req.params.id );
             //or use this 
             //todo.spilce(Number(req.param.id)-1 , 1); not good one because id can be anything
-            fs.writeFile(path , JSON.stringify(filtertodo) , {encoding : "utf-8"} , (err)=>{
-                if(err)
-                {
-                    return res.status(500).json({"message" : "Please try again"});   
+            fs.writeFile(path, JSON.stringify(filtertodo), { encoding: "utf-8" }, (err) => {
+                if (err) {
+                    return res.status(500).json({ "message": "Please try again" });
                 }
-                else{
-                    return res.status(200).json({"status" : "done"})
+                else {
+                    return res.status(200).json({ "status": "done" })
                 }
             })
         })
-    }catch(err){
-        return res.status(500).json({"message" : "Please try agagin"})
+    } catch (err) {
+        return res.status(500).json({ "message": "Please try agagin" })
     }
 })
 
-app.put('/todo/:id' , (req , res)=>{
-    try{
-        fs.readFile(path , {encoding : "utf-8"} , (err , todo)=>{
+app.put('/todo/:id', (req, res) => {
+    try {
+        fs.readFile(path, { encoding: "utf-8" }, (err, todo) => {
             todo = todo ? JSON.parse(todo) : []
             // const idx  = req.params.id ;
             // console.log(todo[Number(idx)])
             const idx = todo.findIndex(todo => todo.id == req.params.id)
-            todo[idx] = {...todo[idx] , ...req.body}
-            fs.writeFile(path , JSON.stringify(todo) , {encoding : "utf-8"} , (err)=>{
-                if(err)
-                {
-                    return res.status(500).json({"message" : "Please try again"});   
+            todo[idx] = { ...todo[idx], ...req.body }
+            fs.writeFile(path, JSON.stringify(todo), { encoding: "utf-8" }, (err) => {
+                if (err) {
+                    return res.status(500).json({ "message": "Please try again" });
                 }
-                else{
-                    return res.status(200).json({"status" : "done"})
+                else {
+                    return res.status(200).json({ "status": "done" })
                 }
             })
         })
-    }catch(err){
-        return res.status(500).json({"message" : "Please try agagin"})
+    } catch (err) {
+        return res.status(500).json({ "message": "Please try agagin" })
     }
 })
-app.listen(3000 , ()=>{
+app.listen(3000, () => {
     console.log("Server is running on port 3000");
 })
