ebb7a9c572526ff4aa1bbb133f07a3adf497107a
Tanuja <tanuja7897@gmail.com>
2025-05-10 23:07:05 +0530

    Change

--- BLOGAPP/server.js
+++ BLOGAPP/server.js
@@ -13,14 +13,7 @@ async function DbConnect(){
     }
 }
 
-const UserSchema = new mongoose.Schema({
-    name : String , 
-    email : {
-        type : String,
-        unique : true
-    } ,
-    password : String     
-})
+
 
 const User = mongoose.model("User" , UserSchema);
 
